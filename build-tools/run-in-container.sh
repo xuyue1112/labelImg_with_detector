db7e78999ef78c7bf0bf4226687e9eeea5ec5fe5
xuyue <xuyue1160@qq.com>
2021-06-06 00:23:27 +0800

    docker shell

--- build-tools/run-in-container.sh
+++ build-tools/run-in-container.sh
@@ -3,11 +3,10 @@ docker run -it \
     --user $(id -u) \
     -e DISPLAY=unix$DISPLAY \
     --workdir=$(pwd) \
-    --volume="/home/$USER:/home/$USER" \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
     --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v "/Users/bytedance/Documents/JiangnaProject/labelImg:/tmp/labelImg" \
     tzutalin/py2qt4
 
